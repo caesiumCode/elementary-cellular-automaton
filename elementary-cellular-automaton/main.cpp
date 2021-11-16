6a8aa66fe0b386cba3f7c76176e168359d4e9ae2
caesiumCode <caesiumcode@aol.com>
2021-11-16 14:54:12 +0300

    Initial Commit

---
+++ elementary-cellular-automaton/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  elementary-cellular-automaton
+//
+//  Created by Stephen Jaud on 16/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
