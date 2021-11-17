f3def591e3a9a84166e38c1a80c411b76c94f76e
caesiumCode <caesiumcode@aol.com>
2021-11-17 14:13:48 +0300

    update default resolution

--- elementary-cellular-automaton/main.cpp
+++ elementary-cellular-automaton/main.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     const sf::Vector2u WINDOW_RESOLUTION(2200, 1500);
-    const sf::Vector2u TEXTURE_RESOLUTION(512, 512);
+    const sf::Vector2u TEXTURE_RESOLUTION(1024, 512);
     
     Program program(WINDOW_RESOLUTION, TEXTURE_RESOLUTION);
     program.run();
